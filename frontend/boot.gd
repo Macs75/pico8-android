887a855fe70d8f969f52be04091499a6e94e64aa
Macs75 <macs75@gmail.com>
2026-05-07 10:17:20 +0200

    #68 a minimal change to start_pico_proot.sh to avoid the force close of the application due the error removing the folder tmp/pulse

--- frontend/boot.gd
+++ frontend/boot.gd
@@ -162,7 +162,7 @@ func _ready() -> void:
 	else:
 		request_storage_permission()
 
-const BOOTSTRAP_PACKAGE_VERSION = "20"
+const BOOTSTRAP_PACKAGE_VERSION = "21"
 
 func setup():
 	set_ui_state(false, false, true) # permission_ui=false, select_zip_ui=false, progress_ui=true
